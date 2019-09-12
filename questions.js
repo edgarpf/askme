d07866cfe855b8d7568af7317cebae1eb1da9ee2
Edgar Peixoto Farrapo <edgar.farrapo@CICCR-CE.INTERNO>
2019-09-12 13:12:43 -0300

    more questions

--- questions.js
+++ questions.js
@@ -1,4 +1,27 @@
 var questions = [			
 	'What is the meaning of life?', 
-	'Do you believe in life after death? Have you ever seen a ghost or do you know someone who saw something?'
+	'Do you believe in life after death? Have you ever seen a ghost or do you know someone who saw something?',
+	'What’s the best comedy movie you’ve ever seen?',
+	'What are some of your long-term goals?',
+	'What’s the most useful thing you own?',
+	'What are some challenges you think the next generation will face?',
+	'What three habits will improve your life?',
+	'For what in your life do you feel most grateful?',
+	'If you could have lunch with one person alive or dead, who would it be?',
+	'What’s the number one item you would save from your burning house?',
+	'What is your favorite thing about your career?',
+	'What does a successful relationship look like to you?',
+	'What goals do you have for the next five years?',
+	'What is the first thing you would do if you won the lottery?',
+	'What is the most useless thing you’ve ever bought?',
+	'What would a world populated by clones of you be like?',
+	'Do you think that aliens exist?',
+	'What are some red flags to watch out for in daily life?',
+	'In the past people were buried with the items they would need in the afterlife, what would you want buried with you so you could use it in the afterlife?',
+	'What’s something people don’t worry about but really should?',
+	'Which charity or charitable cause is most deserving of money?',
+	'If magic was real, what spell would you try to learn first?',
+	'What movie, picture, or video always makes you laugh no matter how often you watch it?',
+	'If you could have an all-expenses paid trip to see any famous world monument, which monument would you choose?',
+	'What about the opposite sex confuses you the most?'
 ];
