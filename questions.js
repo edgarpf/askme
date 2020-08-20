3c5d2c10a3846e58ed3afb926958bc818f305e43
Diego Leal <diegopetrola@gmail.com>
2020-08-20 00:42:13 -0300

    more questions!

--- questions.js
+++ questions.js
@@ -15,13 +15,25 @@ var questions = [
 	'What is the first thing you would do if you won the lottery?',
 	'What is the most useless thing you’ve ever bought?',
 	'What would a world populated by clones of you be like?',
-	'Do you think that aliens exist?',
+	'Do you think that aliens exist? If so, do you think intelligent life exists?',
 	'What are some red flags to watch out for in daily life?',
 	'In the past people were buried with the items they would need in the afterlife, what would you want buried with you so you could use it in the afterlife?',
 	'What’s something people don’t worry about but really should?',
 	'Which charity or charitable cause is most deserving of money?',
-	'If magic was real, what spell would you try to learn first?',
+	'If magic was real, what spell would you try to learn first?', //fireball of course
 	'What movie, picture, or video always makes you laugh no matter how often you watch it?',
 	'If you could have an all-expenses paid trip to see any famous world monument, which monument would you choose?',
-	'What about the opposite sex confuses you the most?'
+	'What about the opposite sex confuses you the most?',
+   'If you could choose to start a new career, which one would it be?',
+   'What is your favorite quote?',
+   'Do you believe the universe has a creator?',
+   'Which high school classes did you hate the most? Which ones did you love the most?', //should we split this into two questions?
+   'Which language, fictional or not, would you like to speak?',
+   'Who inspires you?',
+   'What is your favorite animal?', //too silly maybe?
+   'If synthetic meat became a reality, would you eat it?',
+   'Do you think a machine will ever be able to act or think like a human?',
+   'If you had time and resources to start a new hobby, which one would you choose?',
+   'If a genie offered to fulfill your three wishes, what would you wish for?',
+   'If you could choose a skill to master, which one would you choose?'
 ];
